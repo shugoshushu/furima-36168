05fcb1c87a17adf06f4f3da952a30a71ad618800
shugoshushu <shugo374356@gmail.com>
2021-11-17 13:42:55 +0900

    テーブルの作成

--- db/migrate/20211117030638_devise_create_users.rb
+++ db/migrate/20211117030638_devise_create_users.rb
@@ -32,6 +32,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :nickname, null:false
+      t.string :first_name, null:false
+      t.string :last_name, null:false
+      t.string :kana_first_name, null:false
+      t.string :kana_last_name, null:false
+      t.date :birthday, null:false
+
 
       t.timestamps null: false
     end
