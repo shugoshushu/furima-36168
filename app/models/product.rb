b7d1cac99b1e53d0daaa729065d354b90229ca92
shugoshushu <shugo374356@gmail.com>
2021-11-28 18:08:42 +0900

    ActiveHashの導入とバリデーションの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,24 @@ class Product < ApplicationRecord
   belongs_to :user
   #has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :fee_load
+  belongs_to :send_by
+
+  with_options presence: true do
+    validates :product_name
+    validates :product_explain
+  end
+  
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :fee_load_id
+    validates :prefecture_id
+    validates :send_by_id
+  end
 end
---
+++ app/models/send_by.rb
@@ -0,0 +1,10 @@
+class Send_by < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---'}
+    { id: 2, name: '1〜2日で発送'}
+    { id: 3, name: '2〜3日で発送'}
+    { id: 4, name: '4〜7日で発送'}
+  ]
+
+  has_many :products
+end
