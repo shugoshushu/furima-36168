faba94a314d0d9184209d9db3fd0b2021ba2c4c4
shugoshushu <shugo374356@gmail.com>
2021-11-28 10:01:41 +0900

    ActiveStorageの導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
   def index
     
   end
+
+  private
+
+  def product_params
+    params.repuire(:product).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 
   belongs_to :user
   #has_one :purchase
+  has_one_attached :image
 end
