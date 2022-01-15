207bbda5914b7cf40482f734d5d8c7996feadbcd
shugoshushu <shugo374356@gmail.com>
2022-01-15 19:13:10 +0900

    エラーメッセージの表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,8 +27,12 @@ class ProductsController < ApplicationController
   end
 
   def update
-    product = Product.find(params[:id])
-    product.update(product_params)
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   private
