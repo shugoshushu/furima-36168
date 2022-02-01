77781792bcdfafcb6f497ad51b8a9a24bc4e7570
shugoshushu <shugo374356@gmail.com>
2022-02-01 12:05:26 +0900

    formオブジェクト

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,10 @@
+class PurchasesController < ApplicationController
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   root to: "products#index"
 
-  resources :products
+  resources :products do
+    resources :purchases, only: [:index, :create]
+  end
 
 end
